6fb4c6c625b2f102af7eac77a2e5de022722c823
Brent Rucker <brent.rucker@gmail.com>
2017-08-27 20:46:49 -0700

    updating project

--- nt.c
+++ nt.c
@@ -1,29 +1,59 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+
+void breakStringUp(char *huh, int index, int argc);
+void printNeg(char *huh);
+
 int main(int argc, char *argv[]) {
+
+	if(argc == 1) {
+		printf("\n** Please include numbers after the file name **\n\n");
+		exit(1);
+	}
+
+	/*
+
+	RETURN INVALID NUMBERS!!!!
+
+	*/
+
 	
 	int i;
-	char* arr = malloc(sizeof(char) * argc);
+	//char* arr = malloc(sizeof(char) * argc-1);
 
 	for(i = 0; i < argc; i++) {
-		printf("%s\n",argv[i]);
+		printf("ARR: %s\n",argv[i]);
 		if(i >= 1) {
-			arr[i-1] = *argv[i];
+			char *newStr = argv[i];
+			breakStringUp(newStr, i, argc);
 		}
 	}
-	printf("Argc: %d\n",argc);
-	int j, sLength;
-	for(j = 0; j < argc-1; j++) {
-		sLength = strlen(arr);
-	}
-	/*printf("Number is: %d\n",len);
-	for(i = 0; i < len; i++) {
-		printf()
-	}*/
-
-
-
 	return 0;
 }
+
+
+void breakStringUp(char *huh, int index, int argc) {
+	
+	int len = strlen(huh);
+	int i;
+	printf("Break up: %d\n",argc);
+
+    for(i = 0; i < len; i++) {
+    	if(huh[0] == 45) {
+    		printNeg(huh);
+    		break;
+    	}
+		printf("%c\n",huh[i]);
+	}
+}
+
+
+void printNeg(char *huh) {
+	printf("inside Neg %s\n",huh);
+}
+
+
+
