4f72b8dd9913c7ac72512a20785d0d29ab91e9a7
Brent Rucker <brent.rucker@gmail.com>
2017-08-28 22:14:02 -0700

    updating number converter

--- nt.c
+++ nt.c
@@ -3,50 +3,78 @@
 #include <string.h>
 #include <stdlib.h>
 
+	/*
 
-void breakStringUp(char *huh, int index, int argc);
+	RETURN INVALID NUMBERS!!!!
+	
+	*/
+
+	// get any number and change it to binary and hex
+	// get all values and convert to binary, then binary to everything else
+
+void breakStringUp(char *huh);
 void printNeg(char *huh);
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char *argv[]) {
 
 	if(argc == 1) {
 		printf("\n** Please include numbers after the file name **\n\n");
 		exit(1);
 	}
-
-	/*
-
-	RETURN INVALID NUMBERS!!!!
-
-	*/
-
 	
-	int i;
-	//char* arr = malloc(sizeof(char) * argc-1);
-
+	int i, neg, bin, hex, b, r = 0;
+	
 	for(i = 0; i < argc; i++) {
-		printf("ARR: %s\n",argv[i]);
+
 		if(i >= 1) {
-			char *newStr = argv[i];
-			breakStringUp(newStr, i, argc);
+
+			int len = strlen(argv[i]);
+			printf("uhh %s\n", argv[i]);
+			char* arr = malloc(sizeof(char) * len);
+			strcpy(arr, argv[i]);
+			int j;
+
+			for(j = 0; j < len; j++) {
+				if(arr[0] == 45) {
+					printf("Neg\n");
+					neg++;
+				}
+				if(neg > 0 && arr[1] == 98) {
+					b++;
+					printf("B\n");
+				}
+				if(neg > 0 && arr[1] == 114) {
+					r++;
+					printf("R\n");
+				}
+				printf("%c\n", arr[j]);
+			}
 		}
 	}
+	//breakStringUp(arr);
 	return 0;
 }
 
 
-void breakStringUp(char *huh, int index, int argc) {
+void breakStringUp(char *huh) {
 	
-	int len = strlen(huh);
-	int i;
-	printf("Break up: %d\n",argc);
+	int i, j;
+	int arrLength = strlen(huh);
+	printf("Whaaaa... %s\n",huh);
 
-    for(i = 0; i < len; i++) {
-    	if(huh[0] == 45) {
-    		printNeg(huh);
-    		break;
+    for(i = 0; i < arrLength; i++) {
+
+    	int len = strlen(&huh[i]);
+    	printf("LEN %d\n", len);
+
+    	for(j = 0; j < len; j++) {
+    		if(huh[0] == 45) {
+    			printNeg(huh);
+    			break;
+    		}
+			printf("%c\n",huh[i]);
     	}
-		printf("%c\n",huh[i]);
+    	
 	}
 }
 
